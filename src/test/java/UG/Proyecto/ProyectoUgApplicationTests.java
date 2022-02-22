a5f8ad284cc5a73e38c5cc531cb3078b69b91b10
kelvinVargasUG <joel.vargas.ch@hotmail.com>
2022-02-21 20:48:53 -0500

    ahi mira ya me ire a dormir xd

---
+++ src/test/java/UG/Proyecto/ProyectoUgApplicationTests.java
@@ -0,0 +1,13 @@
+package UG.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
